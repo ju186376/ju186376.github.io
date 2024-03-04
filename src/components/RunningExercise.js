b1a13c176b9ee141c27fe536500fc4ca732dbd8f
Justin Arocha <ju186376@ucf.edu>
2024-03-04 16:05:05 -0500

    Lab 6

--- src/components/RunningExercise.js
+++ src/components/RunningExercise.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react';
 import StopWatch from './StopWatch';
 
 export default function RunningExercise({ exercise, setMenuScreen }){
